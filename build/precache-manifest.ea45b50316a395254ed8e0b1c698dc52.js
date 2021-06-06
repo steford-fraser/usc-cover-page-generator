128f4cf64901e5f0d2a4466c0ee716c554019c59
Steford Fraser <steford_fraser@outlook.com>
2021-06-06 12:46:28 -0400

    include production

---
+++ build/precache-manifest.ea45b50316a395254ed8e0b1c698dc52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e9e7e8ef666710f3246af75238658e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c67112dd838ef04d9d8a",
+    "url": "/static/css/2.b5b33cd6.chunk.css"
+  },
+  {
+    "revision": "c67112dd838ef04d9d8a",
+    "url": "/static/js/2.3aab983c.chunk.js"
+  },
+  {
+    "revision": "4d16db3d9ff9c531a465",
+    "url": "/static/js/main.3be06bd9.chunk.js"
+  },
+  {
+    "revision": "8abd3af84095d8efcafe",
+    "url": "/static/js/runtime-main.357de4cf.js"
+  }
+]);
